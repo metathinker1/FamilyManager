45c478d06904132a2b58b7d5c9205856e6fe52ff
Rob Wood <metathinker1@gmail.com>
2021-02-15 20:59:39 -0700

    print workitemGenRule values read from DB

--- routes/workitem_generators.js
+++ routes/workitem_generators.js
@@ -8,13 +8,43 @@ const WorkitemGenRule = require('../models/workitem_genrule')
 router.post('/', async (req, res) => {
     console.log('workitem_generators: get(): Start')
     try {
-        const workitemGenRules = await WorkitemGenRule.find()
-        
-        workitemGenRules.forEach(function(err, doc) {
-            console.log('Looping: ' + doc)
+        // const workitemGenRules = await WorkitemGenRule.find({})
+        // console.log(workitemGenRules)
+        // // https://stackoverflow.com/questions/41743924/how-can-run-mongoose-query-in-foreach-loop
+        // workitemGenRules.forEach(function(err, doc) {
+        //     console.log(doc.name)
+        //     console.log('Looping: ' + doc)
+        // })
+
+        const cursor = await WorkitemGenRule.find().cursor()
+
+        for (let doc = await cursor.next(); doc != null; doc = await cursor.next()) {
+            console.log(doc.name + ',' + doc.startDate)
+        }
+
+        //  Didn't work
+        // WorkitemGenRule.find({}, 
+        //     '_id name',
+        //     function(err, workitemGenRule) {
+        //         if (!err) {
+        //             if (workitemGenRule) {
+        //                 console.log(workitemGenRule.name)
+        //             }
+        //         }
+        //     }
+        // )
+
+        WorkitemGenRule.findOne({'name': 'Name2'}, 
+        '_id name',
+        function(err, workitemGenRule) {
+            if (!err) {
+                if (workitemGenRule) {
+                    console.log(workitemGenRule.name)
+                }
+            }
         })
 
-        res.json(workitemGenRules)
+        res.json({message: 'Done'})
     } catch(err) {
         res.status(500).json({ message: err.message })
     }
